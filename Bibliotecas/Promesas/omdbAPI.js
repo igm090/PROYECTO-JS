f79bcb1a89abb6a94671dea0806f4ca7bf7b06bf
Isela Garcia <iselagarcia.alu@iespacomolla.es>
2022-02-07 23:54:37 +0100

    v.0.0.1 - Información API

---
+++ Bibliotecas/Promesas/omdbAPI.js
@@ -0,0 +1,9 @@
+/**
+ *  ejemplo json - guardianes de la galaxia 2 http://www.omdbapi.com/?i=tt3896198&apikey=1d78ab91
+ * 
+ *  documentación http://www.omdbapi.com/
+ *  url posters http://img.omdbapi.com/?apikey=1d78ab91&
+ *  url data requests http://www.omdbapi.com/?apikey=1d78ab91&
+ */
+
+const apiKey = "&apikey=1d78ab91"; //añadir al final de cada url
